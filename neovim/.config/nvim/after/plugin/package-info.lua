419e31712d943b6f8f8de98199eb3196bfa7916b
adrigzr <a.gonzalezrus@gmail.com>
2026-04-22 09:02:32 +0200

    nvim: use bare string call in package-info diffview guard

--- neovim/.config/nvim/after/plugin/package-info.lua
+++ neovim/.config/nvim/after/plugin/package-info.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
     -- diffview.nvim opens a virtual buffer (diffview:// URI) for package.json,
     -- because the derived cwd is not a valid filesystem directory.
     local bufname = vim.api.nvim_buf_get_name(0)
-    if bufname:match("^diffview://") then
+    if bufname:match "^diffview://" then
       return
     end
     package_info.show()
